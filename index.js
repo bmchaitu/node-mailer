17f51455a0a3a0a021b4d8146aaa4c39f308cb3d
iamfine420 <budagavichaitu@gmail.com>
2019-08-05 23:32:32 +0530

    Node mailer

---
+++ index.js
@@ -0,0 +1,26 @@
+var nodemailer = require('nodemailer');
+var xoauth2 = require('xoauth2');
+ var smtpTransport = nodemailer.createTransport({
+ 	service:'Gmail',
+ 	auth:{
+ 		//xoauth2: xoauth2.createXOAuth2Generator({
+ 		//
+    user : 'budagavichaitu@gmail.com',
+ 		pass : 'tomandjerry@CN420',
+ 		}
+ 	//}
+ });
+  var mailOptions = {
+  	from : 'budagavichaitu@gmail.com',
+  	to : 'hemabasavaraj.459@gmail com',
+  	subject : "Testing mail",
+  	text : "Hello World",
+  	html : "<h1>HelloWorld</h1>"
+  };
+
+  smtpTransport.sendMail(mailOptions,(error,response) => {
+  	if(error)
+  	console.log('Error'+error.message);
+  	else
+  	console.log('success'+response.message);
+  });
